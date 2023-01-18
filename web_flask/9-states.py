234370957e8c20a64228a3ff814753cb69a3ad34
0498drew <mwaseandrew98@gmail.com>
2023-01-18 15:10:42 -0800

    task9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,20 +9,24 @@ from models import storage
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<state_id>', strict_slashes=False)
-def states(state_id=None):
-    """display the states and cities listed in alphabetical order"""
-    states = storage.all("State")
-    if state_id is not None:
-        state_id = 'State.' + state_id
-    return render_template('9-states.html', states=states, state_id=state_id)
-
-
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def exeption(e):
+    """ close session """
     storage.close()
+
+
+@app.route('/states')
+@app.route('/states/<id>')
+def StatesAndcitiesByState(id=None):
+    """ Handle route """
+    state = storage.all("State")
+    if id is None:
+        return render_template('9-states.html', state=state)
+    else:
+        for x in state.values():
+            if x.id == id:
+                return render_template("9-states.html", state=x)
+    return render_template("9-states.html")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
